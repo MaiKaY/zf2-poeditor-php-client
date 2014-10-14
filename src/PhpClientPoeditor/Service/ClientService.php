2f5d7b7b27c257d8ee6da95bc29b6a41e275f666
MaiKaY <maik.schmidt.hl@gmail.com>
2014-10-14 08:27:56 +0200

    add logic to PhpClientPoeditor\Service\ClientService

--- src/PhpClientPoeditor/Service/ClientService.php
+++ src/PhpClientPoeditor/Service/ClientService.php
@@ -26,14 +26,33 @@
 namespace PhpClientPoeditor\Service;
 
 use PhpClientPoeditor\Options\Options;
+use PhpClientPoeditor\Strategy\StrategyInterface;
+use Zend\Http;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
 /**
  * PhpClientPoeditor\Service\ClientService
  */
-class ClientService
+class ClientService implements ServiceLocatorAwareInterface
 {
+    use ServiceLocatorAwareTrait;
+
     /** @var Options */
     private $options;
+    /** @var array */
+    private $validTypes = array(
+        'po',
+        'pot',
+        'mo',
+        'xls',
+        'apple_strings',
+        'android_strings',
+        'resx',
+        'resw',
+        'properties',
+        'json',
+    );
 
     /**
      * @param Options $options
@@ -41,5 +60,116 @@ class ClientService
     public function __construct(Options $options)
     {
         $this->options = $options;
+    }
+
+    /**
+     * @return void
+     * @throws \DomainException
+     */
+    public function build()
+    {
+        foreach ($this->options->getStrategies() as $contentType => $fqnStrategy) {
+            if (!$this->isValidType($contentType)) {
+                throw new \DomainException('Invalid Type (' . $contentType . ')');
+            }
+            $strategy = $this->getStrategy($fqnStrategy);
+            foreach ($this->options->getLanguages() as $languageKey => $languageProjectKey) {
+                $data = array(
+                    'type'     => $contentType,
+                    'language' => $languageKey,
+                );
+                $content = file_get_contents($this->getFile($data));
+                $content = $strategy->build($content);
+
+                $file = strtolower($languageProjectKey) . '.' . $contentType;
+                $this->writeFile($strategy->getSavePath() . '/' . $file, $content);
+            }
+        }
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return string
+     * @throws \RuntimeException
+     */
+    private function getFile(array $data)
+    {
+        $data = array_merge(
+            array(
+                'api_token' => $this->options->getApiToken(),
+                'action'    => 'export',
+            ),
+            $data
+        );
+
+        $adapter = new Http\Client\Adapter\Curl;
+        $client = new Http\Client;
+        $client->setAdapter($adapter);
+
+        $request = new Http\Request;
+        $request->setUri($this->options->getUrl());
+        $request->setMethod(Http\Request::METHOD_POST);
+        $request->setContent(http_build_query($data));
+
+        /** @var \Zend\Http\Response $response */
+        $response = $client->dispatch($request);
+
+        if (!$response->isSuccess()) {
+            throw new \RuntimeException('Request was not successful');
+        }
+
+        /** @var \stdClass $content */
+        $content = json_decode($response->getContent());
+        if ($content->response->code != 200) {
+            throw new \RuntimeException($content->response->message);
+        }
+
+        return $content->item;
+    }
+
+    /**
+     * @param string $file
+     * @param string $data
+     *
+     * @return void
+     */
+    private function writeFile($file, $data)
+    {
+        $handler = fopen($file, 'w+');
+
+        if ($handler !== false) {
+            fwrite($handler, $data, strlen($data));
+            fflush($handler);
+            fclose($handler);
+            chmod($file, 0777);
+        }
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return bool
+     */
+    private function isValidType($type)
+    {
+        return in_array($type, $this->validTypes);
+    }
+
+    /**
+     * @param string $alias
+     *
+     * @return StrategyInterface
+     * @throws \RuntimeException
+     */
+    private function getStrategy($alias)
+    {
+        $strategy = $this->getServiceLocator()->get($alias);
+        if (!$strategy instanceof StrategyInterface) {
+            throw new \RuntimeException(
+                'Requested Strategy must implement PhpClientPoeditor\Strategy\StrategyInterface'
+            );
+        }
+        return $strategy;
     }
 }
