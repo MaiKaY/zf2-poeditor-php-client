1b993bf535d6fe410f21046acbd08ac57685e7cc
MaiKaY <maik.schmidt.hl@gmail.com>
2014-10-23 07:50:11 +0200

    add PhpClientPoeditorTest\Strategy\JsonKeyValueStrategyTest

---
+++ tests/PhpClientPoeditorTest/Strategy/JsonKeyValueStrategyTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Maik Schmidt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PhpClientPoeditorTest\Strategy;
+
+use PhpClientPoeditor\Strategy\JsonKeyValueStrategy;
+
+/**
+ * PhpClientPoeditorTest\Strategy\JsonKeyValueStrategyTest
+ */
+class JsonKeyValueStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var JsonKeyValueStrategy */
+    protected $fixture;
+
+    public function setUp()
+    {
+        $this->fixture = new JsonKeyValueStrategy;
+    }
+
+    public function testImplementsStrategyInterface()
+    {
+        $this->assertInstanceOf('PhpClientPoeditor\Strategy\StrategyInterface', $this->fixture);
+    }
+
+    public function testBuild()
+    {
+        $translation = new \stdClass;
+        $translation->term = 'foo';
+        $translation->definition = 'bar';
+        $translations = array($translation);
+
+        $expected = json_encode(
+            array(
+                'foo' => 'bar',
+            )
+        );
+
+        $result = $this->fixture->build(json_encode($translations));
+        $this->assertSame($expected, $result);
+        $this->assertTrue(is_string($result));
+    }
+}
